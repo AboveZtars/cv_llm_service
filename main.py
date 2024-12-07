6abac2915901ceba1ba70a58fdbd016e81e9cf55
Rafael Molina <rafael@molina-aquino.com>
2024-12-07 13:23:56 -0400

    Feature add limiter

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
 from dotenv import load_dotenv
 from rafa_chat_engine import chat_engine
 from fastapi.middleware.cors import CORSMiddleware
 from prompts import context_input_prompt
+from slowapi import Limiter, _rate_limit_exceeded_handler
+from slowapi.util import get_remote_address
+from slowapi.errors import RateLimitExceeded
+
 
 load_dotenv()
 
+limiter = Limiter(key_func=get_remote_address,  default_limits=["1/minute"])
 app = FastAPI()
 
 # Add CORS middleware
@@ -17,6 +22,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+app.state.limiter = limiter
+app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 
 class Message(BaseModel):
@@ -29,10 +37,19 @@ def read_root():
 
 
 @app.post("/openai")
-def get_openai_response(message: Message):
+@limiter.limit("5/30seconds")
+async def get_openai_response(request: Request):
     try:
+        # Parse the JSON body from the request
+        body: Message = await request.json()
+        user_message = body.user_message
+
+        if not user_message:
+            raise HTTPException(
+                status_code=400, detail="user_message is required")
+
         response = chat_engine.chat(
-            context_input_prompt + message.user_message)
+            context_input_prompt + user_message)
 
         # Log the response
         print(f"Response: {response.response}")
