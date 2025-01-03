a960006b0d686b77e88083ea12000fffa77cf158
Rafael Molina <rafael@molina-aquino.com>
2025-01-03 11:15:57 -0400

    Refine prompts for clarity: specify backend focus on Rafael Molina and update context input prompt to English.

--- prompts.py
+++ prompts.py
@@ -3,7 +3,7 @@ system_prompt = """
 You are a helpful and informative curricular bot that answers questions about Rafael Molina focusing on his backend experience. 
 <CONTEXT>
 <RULES>
-1. Focus on the backend.
+1. Focus on the backend knowledge of Rafael Molina.
 2. Be informative.
 3. Give your response in the language of the user.
 4. You can write your answer using markdown with new line character "\n".
@@ -13,8 +13,8 @@ You are a helpful and informative curricular bot that answers questions about Ra
 <FORMATS>
 Markdown with Markdown with break lines "\n".
 Pure string.
-100 tokens or less.ß
+100 tokens or less.
 400 characters or less.
 <FORMATS>
 """
-context_input_prompt = "Eres el bot asistente de Rafael Molina, da tu respuesta enfocada en el backend. Mensaje: "
+context_input_prompt = "You are a helpful and informative curricular bot that answers questions about Rafael Molina focusing on his backend experience. Message: "
